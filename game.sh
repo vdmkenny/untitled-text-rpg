409f3afb86d6346bf8676b27e1f1e8c006e98102
Kenny Van de Maele <vdmkenny@inuits.eu>
2016-10-06 15:07:51 +0200

    basic command interpretor
    Signed-off-by: Kenny Van de Maele <vdmkenny@inuits.eu>

--- game.sh
+++ game.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# MUD Game by Kenny Van de Maele
+# Text-based RPG by Kenny Van de Maele
 #
 
 #Main Vars
 GAMENAME="Placeholder title"
 VERSION="v0.0.1a"
-DEBUG=true
+DEBUG=false
 GAMELOOP=true
 
 #Game Vars
@@ -31,15 +31,55 @@ function playercommand {
     fi
     return
   fi
+  #normalise command
+  #COMMAND=$(echo $1 | tr -dc '[:alnum:]' | tr '[:upper:]' '[:lower:]')
+
   if $DEBUG; then 
-    echo -e "${GREEN}[DEBUG] ${NC}Command issued: $1"
+    echo -e "${GREEN}[DEBUG] ${NC}Command issued: $COMMAND"
   fi
+ 
+  #process player command
+
+  #check for multiple words
+  if [[ $COMMAND == *" "* ]]; then
+    WORD1=$(echo $COMMAND | cut -d " " -f 1)
+    WORD2=$(echo $COMMAND | cut -d " " -f 2)
+    WORD3=$(echo $COMMAND | cut -d " " -f 3)
+  else
+    WORD1=$COMMAND
+  fi
+
+  case $WORD1 in
+    look )
+      case $WORD2 in
+        "" ) getroomdescription ; return;;
+        at ) 
+          case $WORD3 in
+            "" ) echo "look at what?"; return 1;;
+            floor|ground ) echo "It's dirty."; return ;;
+            sky ) echo "It's blue."; return ;;
+            room|area ) getroomdescription; return ;;
+          esac
+          ;;
+        * )
+      esac
+      ;;
+    * ) echo "I beg you pardon?"; return 1;;
+  esac 
+
 }
 
-function getroom {
-  echo  
+function getroomdescription {
+  #which map are we on?
+  case "$CURRENTMAP,$CURRENTX,$CURRENTY" in
+    "overworld,0,0" ) echo "You find yourself in the starting area." ;;
+     * ) echo "${RED}[ERROR] ${NC}Invalid room. You should not be here." return; ;;
+  esac
 }
 
+COMMAND=""
+#clear the terminal
+clear
 #New Game Introduction
 echo -e "Welcome to ${RED}$GAMENAME $VERSION!${NC}"
 #check if we are in debug
@@ -47,10 +87,6 @@ if $DEBUG; then
   echo -e "${GREEN}Debug mode enabled${NC}"
 fi
 echo
-
-COMMAND=""
-#clear the terminal
-clear
 
 #get playername
 echo "What is your name, adventurer?"
@@ -67,13 +103,13 @@ PLAYERNAME="${BLUE}$PLAYERNAME${NC}"
 echo
 echo -e "Hello, $PLAYERNAME!"
 
-
 #Main gameloop
+echo "What will you do?"
+read -rp ">" COMMAND
+playercommand $COMMAND
 while $GAMELOOP; do
-  echo "What will you do?"
   read -rp ">" COMMAND
- 
-  playercommand $COMMAND
+  if playercommand $COMMAND; then echo "What will you do?"; fi
 
   if $DEBUG; then 
     echo -e "${GREEN}[DEBUG] ${NC}Player location: $CURRENTMAP,$CURRENTX,$CURRENTY"
